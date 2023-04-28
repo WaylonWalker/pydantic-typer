9be9a1ff36fe581a0cde78bf1c3091482910e4e9
Waylon S. Walker <waylon@waylonwalker.com>
2023-04-28 08:33:06 -0500

    setup pyannotate

--- examples/person.py
+++ examples/person.py
@@ -1,3 +1,4 @@
+from pyannotate_runtime import collect_types
 
 from examples.models import Person
 from pydantic_typer import expand_pydantic_args
@@ -9,5 +10,12 @@ def get_person(person: Person, thing: str = None) -> Person:
     from rich import print
 
     print(str(thing))
-
     print(person)
+
+
+if __name__ == "__main__":
+    collect_types.init_types_collection()
+    with collect_types.collect():
+        person = get_person(name="John", age=1, r=1, g=1, b=1, a=1, length=1)
+
+    collect_types.dump_stats("type_info.json")
